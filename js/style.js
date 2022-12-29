ee6fe65aed2b6c6853ed20a9d4207d69471cdb59
Adivya Jain <95540661+adivya-jain@users.noreply.github.com>
2022-12-29 05:35:23 +0530

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,136 @@
+let searchForm = document.querySelector('.search-form')
+
+document.querySelector('#search-btn').onclick = () => {
+  searchForm.classList.toggle('active');
+  Navbar.classList.remove('active');
+  shoppingCart.classList.remove('active');
+  LoginForm.classList.remove('active');
+
+}
+
+let shoppingCart = document.querySelector(".shopping-cart")
+
+document.querySelector('#cart-btn').onclick = () => {
+  shoppingCart.classList.toggle('active');
+  Navbar.classList.remove('active');
+  LoginForm.classList.remove('active');
+  searchForm.classList.remove('active');
+}
+
+let LoginForm = document.querySelector('.login-form')
+
+document.querySelector('#login-btn').onclick = () => {
+  LoginForm.classList.toggle('active');
+  Navbar.classList.remove('active');
+  shoppingCart.classList.remove('active');
+  searchForm.classList.remove('active');
+}
+
+let Navbar = document.querySelector('.navbar')
+
+document.querySelector('#menu-btn').onclick = () => {
+  Navbar.classList.toggle('active');
+  shoppingCart.classList.remove('active');
+  LoginForm.classList.remove('active');
+  searchForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+  Navbar.classList.remove('active');
+  shoppingCart.classList.remove('active');
+  LoginForm.classList.remove('active');
+  searchForm.classList.remove('active');
+}
+
+
+var swiper = new Swiper(".products-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+
+    },
+    1020: {
+      slidesPerView: 3,
+
+    },
+  },
+});
+
+
+
+var swiper = new Swiper(".reviews-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+
+    },
+    1020: {
+      slidesPerView: 3,
+
+    },
+  },
+});
+
+
+function loader() {
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+function fadeOut() {
+  setInterval(loader, 1500);
+}
+window.onload = fadeOut;
+
+
+
+
+
+
+
+
+
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (!user) {
+    location.replace("index2.html")
+  }
+  else {
+    document.getElementById("user-login").innerHTML = `Hello, ${user.email}`;
+    // let LOGOUT=document.querySelector("#logout")
+    // document.querySelector('logout').onclick=()=>{
+    //   LOGOUT.classList.toggle('active');}
+
+
+  }
+})
+
+
+function logout()
+{
+firebase.auth().signOut()
+}
+function guest()
+{
+  document.getElementById("user-login").innerHTML = "Hello," + "Guest"
+  location.replace("index1.html")
+
+}
---
+++ js/style2.js
@@ -0,0 +1,34 @@
+document.getElementById("SIGNUP").addEventListener("submit", (event) => {
+    event.preventDefault()
+})
+
+
+
+function SignUp() {
+    // const Name = document.getElementById("name").value;
+    
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    firebase.auth().signInWithEmailAndPassword(email,password);
+    
+    // .catch(error){
+    //     document.getElementById("error").innerHTML=error.message;
+    // };
+    // saveMessage(Name, email, password);
+    // document.getElementById('SIGNUP').reset();
+}
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        location.replace("index.html");
+    }
+})
+
+
+// function saveMessage(Name, email, password) {
+//     var newMessageRef = messagesRef.push();
+//     newMessageRef.set({
+//         name: Name,
+//         email: email,
+//         pass: password
+//     });
+// }
